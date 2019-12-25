a63163e4a9024831f5e1ce46aeb8fc931b0b9908
Eric Ren <546296+rencire@users.noreply.github.com>
2019-12-25 15:01:24 -0800

    chore: formatter

--- shell.nix
+++ shell.nix
@@ -1,6 +1,9 @@
 with import ./nix {};
   mkShell { 
     buildInputs = [ 
+      # cross platform tool for managing git hooks
       lefthook
+      # used for 'prettier tool for formatting markdown files
+      nodejs-10_x
     ];
   }
